7b636f888293d2f71510f10944df1dc130ece902
TheoLisin <theo.lisin@gmail.com>
2023-06-11 11:27:31 +0300

    change folder name

--- serve/serve/src/sly_functions.py
+++ serve/serve/src/sly_functions.py
@@ -6,14 +6,20 @@ from cv2 import Mat
 
 class SupportedModels(Enum):
     VIT: str = 'mixformer_vit_online'
-    CONVMAE: str = 'mixformer_convmae_online' 
+    CONVMAE: str = 'mixformer_convmae_online'
+
+    @classmethod
+    def instance_by_name(cls, name):
+        if "vit" in name:
+            return SupportedModels.VIT
+        return SupportedModels.CONVMAE
 
 
 class Tracker(object):
     def __init__(
         self, 
         name: SupportedModels,
-        search_area_scale: float,
+        search_area_scale: float = 4.5,
     ) -> None:
         self.parameter_name = 'baseline_large'
         if name is SupportedModels.VIT:
@@ -21,13 +27,15 @@ class Tracker(object):
         else:
             self.model = 'mixformer_convmae_large_online.pth.tar'
         
-        tracker_module = importlib.import_module('lib.test.tracker.{}'.format(name))
+        tracker_module = importlib.import_module('lib.test.tracker.{}'.format(name.value))
         tracker_class = tracker_module.get_tracker_class()
-        param_module = importlib.import_module('lib.test.parameter.{}'.format(name))
+        param_module = importlib.import_module('lib.test.parameter.{}'.format(name.value))
         params = param_module.parameters(self.parameter_name, self.model, search_area_scale)
-        self.tracker = tracker_class(params, "lasot")
+        # params.update_interval = 20
+        # params.online_sizes = 5
+        self.tracker = tracker_class(params, "vot20")
     
-    def initialize(self, rgb_image: Mat, x: int, y: int, h: int, w: int):
+    def initialize(self, rgb_image: Mat, x: int, y: int, w: int, h: int):
         init_info = {"init_bbox": [x, y, w, h]}
         self.tracker.initialize(rgb_image, init_info)
     
@@ -41,3 +49,6 @@ class Tracker(object):
         """
         out = self.tracker.track(rgb_image, {})
         return out['target_bbox']
+    
+    def update_init(self, rgb_image: Mat, x: int, y: int, h: int, w: int):
+        self.initialize(rgb_image, x, y, h, w)
