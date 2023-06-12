47d61dd341e51efc5025863c2cb8c6bf11605326
TheoLisin <theo.lisin@gmail.com>
2023-06-12 08:43:28 +0000

    requirements upd

--- install_reqs.sh
+++ install_reqs.sh
@@ -7,98 +7,100 @@ set -e
 echo ""
 echo ""
 echo "****************** Installing yaml ******************"
-pip install PyYAML
+pip3 install PyYAML
 
 echo ""
 echo ""
 echo "****************** Installing easydict ******************"
-pip install easydict
+pip3 install easydict
 
 echo ""
 echo ""
 echo "****************** Installing cython ******************"
-pip install cython
+pip3 install cython
 
-echo ""
-echo ""
-echo "****************** Installing opencv-python ******************"
-pip install opencv-python
+# echo ""
+# echo ""
+# echo "****************** Installing opencv-python ******************"
+# pip3 install opencv-python
 
 echo ""
 echo ""
 echo "****************** Installing pandas ******************"
-pip install pandas
+pip3 install pandas
 
-echo ""
-echo ""
-echo "****************** Installing tqdm ******************"
-conda install -y tqdm
+# echo ""
+# echo ""
+# echo "****************** Installing tqdm ******************"
+# conda install -y tqdm
 
 echo ""
 echo ""
 echo "****************** Installing coco toolkit ******************"
-pip install pycocotools
+pip3 install pycocotools
 
 echo ""
 echo ""
 echo "****************** Installing jpeg4py python wrapper ******************"
 sudo apt-get install libturbojpeg
-pip install jpeg4py
+pip3 install jpeg4py
 
 echo ""
 echo ""
 echo "****************** Installing tensorboard ******************"
-pip install tb-nightly
+pip3 install tb-nightly
 
 echo ""
 echo ""
 echo "****************** Installing tikzplotlib ******************"
-pip install tikzplotlib
+pip3 install tikzplotlib
 
 echo ""
 echo ""
 echo "****************** Installing thop tool for FLOPs and Params computing ******************"
-pip install --upgrade git+https://github.com/Lyken17/pytorch-OpCounter.git
+pip3 install --upgrade git+https://github.com/Lyken17/pytorch-OpCounter.git
 
 echo ""
 echo ""
 echo "****************** Installing colorama ******************"
-pip install colorama
+pip3 install colorama
 
 echo ""
 echo ""
 echo "****************** Installing lmdb ******************"
-pip install lmdb
+pip3 install lmdb
 
 echo ""
 echo ""
 echo "****************** Installing scipy ******************"
-pip install scipy
+pip3 install scipy
 
 echo ""
 echo ""
 echo "****************** Installing visdom ******************"
-pip install visdom
+pip3 install visdom
 
 echo ""
 echo ""
 echo "****************** Installing vot-toolkit python ******************"
-pip install git+https://github.com/votchallenge/vot-toolkit-python
+pip3 install git+https://github.com/votchallenge/vot-toolkit-python
 
 echo ""
 echo ""
 echo "****************** Installing onnx and onnxruntime-gpu ******************"
-pip install onnx onnxruntime-gpu==1.6.0
+pip3 install onnx onnxruntime-gpu==1.6.0
 
 echo ""
 echo ""
 echo "****************** Installing timm ******************"
-pip install timm==0.4.12
+pip3 install timm==0.4.12
 
 echo "****************** Installing yacs/einops/thop ******************"
-pip install yacs
-pip install einops
-pip install thop
+pip3 install yacs
+pip3 install einops
+pip3 install thop
+
+pip3 install supervisely==6.72.31
 
 echo "****************** Install ninja-build for Precise ROI pooling ******************"
 sudo apt-get install ninja-build
