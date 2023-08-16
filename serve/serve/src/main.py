ecd97251fdb662d8e180f268fceacf8b7efa1ca8
Theo <theo.lisin@gmail.com>
2023-08-16 18:51:45 +0300

    fix env set

--- serve/serve/src/main.py
+++ serve/serve/src/main.py
@@ -17,8 +17,8 @@ from supervisely.nn.prediction_dto import PredictionBBox
 
 
 NAME = os.environ.get("modal.state.modelName", "mixformer_vit_online")
-os.environ["SMART_CACHE_TTL"] = 5 * 60
-os.environ["SMART_CACHE_SIZE"] = 512
+os.environ["SMART_CACHE_TTL"] = str(5 * 60)
+os.environ["SMART_CACHE_SIZE"] = str(512)
 
 root = (Path(__file__).parent / ".." / ".." / "..").resolve().absolute()
 
