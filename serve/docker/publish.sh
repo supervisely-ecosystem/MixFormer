22f9e160208994adf55981764b52d1cb8f50edbb
vorozhkog <vorozhkog@icloud.com>
2026-02-10 06:02:42 +0000

    Update Docker image tag to 1.0.16 in publish script

--- serve/docker/publish.sh
+++ serve/docker/publish.sh
@@ -1,5 +1,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/../../"
-docker build -t supervisely/mixformer:1.0.14 -f serve/docker/Dockerfile . && \
-docker push supervisely/mixformer:1.0.14
+docker build -t supervisely/mixformer:1.0.16 -f serve/docker/Dockerfile . && \
+docker push supervisely/mixformer:1.0.16
